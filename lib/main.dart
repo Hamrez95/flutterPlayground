f2933ae9fb8bdb13c672f1291a5dfc1d3f1c257e
Hamidreza Pakpour <Hamidrezapakpour95@gmail.com>
2025-03-06 21:06:41 +0330

    rich simple app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
+  runApp(
+    MaterialApp(
+        debugShowCheckedModeBanner: false, // This removes the debug banner
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('I Am Rich'),
+            centerTitle: true,
+            titleTextStyle: TextStyle(color: Colors.white),
+            backgroundColor: const Color.fromARGB(255, 32, 32, 32),
+          ),
+          body:
+             Image(
+              image: AssetImage('images/rich.jpeg'),
+               fit: BoxFit.cover, // This makes the image fill the body
+               width: double.infinity, // Ensures it stretches fully
+               height: double.infinity, // Ensures it stretches fully
+          ),
+          backgroundColor: const Color.fromARGB(255, 0, 44, 37),
+        )),
+  );
 }
-
