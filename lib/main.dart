07871c61351d78ac2745049db6f0b84c2b9ac884
Hamidreza Pakpour <Hamidrezapakpour95@gmail.com>
2025-03-08 15:45:27 +0330

    use ListTail beside Row in card widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,81 +12,82 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-              child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/hamidProfile.jpg'),
-                radius: 50,
-              ),
-              Text(
-                  'Hamidreza Pakpour',
-              style:TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40,
-                fontWeight:FontWeight.w400,
-                color: Colors.white
-              ) ,
-              ),
-              SizedBox(height: 10,),
-              Text(
-                'DEVELOPER',
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/hamidProfile.jpg'),
+                  radius: 50,
+                ),
+                Text(
+                    'Hamidreza Pakpour',
                 style:TextStyle(
-                    fontFamily: 'PressStart2P',
-                    fontSize: 10,
-                    letterSpacing: 2.5,
-                    color: Colors.teal.shade100
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  fontWeight:FontWeight.w400,
+                  color: Colors.white
                 ) ,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Card(
+                ),
+                SizedBox(height: 10,),
+                Text(
+                  'DEVELOPER',
+                  style:TextStyle(
+                      fontFamily: 'PressStart2P',
+                      fontSize: 10,
+                      letterSpacing: 2.5,
+                      color: Colors.teal.shade100
+                  ) ,
+                ),
+                    SizedBox(
+                      height: 20,
+                      width: 150,
+                      child: Divider(
+                        color: Colors.teal.shade100,
+                      ),
+                    ),
+
+                    Card(
                   color: Colors.white,
                   margin: EdgeInsets.symmetric(vertical: 20,horizontal: 50),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                          Icons.phone,
-                      color: Colors.teal.shade900,),
-                      SizedBox(width: 5),
-                      Text(
-                        '09195515233',
-                        style: TextStyle(
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1,
-                          color: Colors.teal.shade900),),
-                 ],
-                ),
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 0,horizontal: 50),
-                padding:EdgeInsets.symmetric(vertical: 10,horizontal: 20) ,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal.shade900,),
-                    SizedBox(width: 5),
-                    Text(
-                      'Hamidrezapakpour95@gmail.com',
+                  child: ListTile(
+                    leading:Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900),
+                    title: Text(
+                      '09195515233',
                       style: TextStyle(
                           fontFamily: 'Source Sans Pro',
                           fontSize: 12,
                           fontWeight: FontWeight.bold,
                           letterSpacing: 1,
-                          color: Colors.teal.shade900),),
-                  ],
+                          color: Colors.teal.shade900),
+
                 ),
+                ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 0,horizontal: 50),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color:Colors.teal.shade900),
+                    title:Text('Hamidrezapakpour95@gmail.com',
+                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                            color: Colors.teal.shade900),
+                    ),
+                  ),
+                )
+                    ],
+                  ),
+              ),
               )
-
-
-            ],
           ),
-          ),
-        ),
       );
   }
 }
