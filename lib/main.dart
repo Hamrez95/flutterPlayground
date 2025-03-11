549bb6aad661b2a05b31d0b87c35587c9eeb7117
Hamidreza Pakpour <Hamidrezapakpour95@gmail.com>
2025-03-11 13:31:28 +0330

    functions and string interpulation

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var leftButtonNumber = 3;
     return Center(
       child: Row(
         children: <Widget> [
@@ -26,7 +27,7 @@ class DicePage extends StatelessWidget {
             onPressed: (){
               print('left button got pressed');
           },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftButtonNumber.png'),
           ),
           ),
           Expanded(
