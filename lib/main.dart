df58c32043d3df468f34b916a57acea84bdf71f1
Hamidreza Pakpour <Hamidrezapakpour95@gmail.com>
2025-07-25 00:30:52 +0330

    blog club main page stories

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,150 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hamrez_flutter_playground/data.dart';
 
 void main() {
-
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  static const defaultFontFamily = "Avenir";
+
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryTextColor = Color(0xff0D253C);
+    final secondaryTextColor = Color(0xff2D4379);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Test',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+              titleSmall: TextStyle(
+                  fontFamily: defaultFontFamily,
+                  color: secondaryTextColor,
+                  fontSize: 14),
+              bodyMedium: TextStyle(
+                  fontFamily: defaultFontFamily,
+                  color: secondaryTextColor,
+                  fontSize: 12),
+              headlineSmall: TextStyle(
+                  fontFamily: defaultFontFamily,
+                  fontWeight: FontWeight.bold,
+                  color: primaryTextColor))),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final stories = AppDatabase.stories;
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Jonothan!',
+                      style: themeData.textTheme.titleSmall,
+                    ),
+                    Image.asset(
+                      'assets/img/icons/notification.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 24),
+                child: Text(
+                  "Explore today's",
+                  style: themeData.textTheme.headlineSmall,
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: ListView.builder(
+                    itemCount: stories.length,
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                    itemBuilder: (context, index) {
+                      final story = stories[index];
+
+                      return Container(
+                        margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  width: 68,
+                                  height: 68,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(24),
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.topLeft,
+                                        colors: [
+                                          Color(0xff376AED),
+                                          Color(0xff49B0E2),
+                                          Color(0xff9CECFB)
+                                        ]),
+                                  ),
+                                  child: Container(
+                                    margin: const EdgeInsets.all(3),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: Colors.white,
+                                    ),
+                                    padding: const EdgeInsets.all(4),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(16),
+                                      child: Image.asset(
+                                          'assets/img/stories/${story.imageFileName}'),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Image.asset(
+                                    'assets/img/icons/${story.iconFileName}',
+                                    width: 24,
+                                    height: 24,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              story.name,
+                              style: themeData.textTheme.bodyMedium,
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
